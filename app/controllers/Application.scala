cc4a890efac5bfeff5712317d114d14f291f7b0b
Jose Sanchez <jsanchez@gilt.com>
2013-03-03 22:45:38 -0500

    added a welcome with name drop down to nav bar

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,25 +14,33 @@ object Application extends Controller with Secured {
 
   def home = IsAuthenticated { username => _ =>
     User.findByEmail(username).map { user =>
+      authenticatedUser.name = user.firstName
       Ok(views.html.index("Next Game"))
     }.getOrElse(Redirect(routes.Login.login))
   }
 
   def schedule = IsAuthenticated { username => implicit request =>
     User.findByEmail(username).map { user => 
-      Ok(views.html.schedule("Winter 2013 Season", Schedule.getGames))
+      authenticatedUser.name = user.firstName
+      Ok(views.html.schedule("Winter 2013 Season", Schedule.getGames)(user))
     }.getOrElse(Redirect(routes.Login.login))
   }
 
   def roster = IsAuthenticated { username => _ =>
     User.findByEmail(username).map { user =>
+      authenticatedUser.name = user.firstName
       Ok(views.html.roster("Gilt Unit"))
     }.getOrElse(Redirect(routes.Login.login))
   }
 
   def news = IsAuthenticated { username => _ =>
     User.findByEmail(username).map { user =>
+      authenticatedUser.name = user.firstName
       Ok(views.html.news("News & Highlights"))
     }.getOrElse(Redirect(routes.Login.login))
+  }
+
+  object authenticatedUser {
+    var name = ""
   }
 }
