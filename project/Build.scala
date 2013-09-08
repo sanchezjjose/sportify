5731f3622cc329b28811ea75c1f20eafe974b231
Jose Sanchez <sanchezjjose@gmail.com>
2013-09-08 01:14:11 -0400

    renaming project to sportify

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "svc-gilt-sports"
+    val appName         = "sportify"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
