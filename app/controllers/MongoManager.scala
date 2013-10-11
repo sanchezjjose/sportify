66c8e1864080812c140eb783911f402903d0d13c
Jose Sanchez <jsanchez@gilt.com>
2013-10-11 00:36:21 -0400

    another mongo attempt

--- app/controllers/MongoManager.scala
+++ app/controllers/MongoManager.scala
@@ -10,7 +10,7 @@ import com.mongodb.casbah.Imports._
 
 
 object MongoManager {
-  val mongoConn = MongoClient(new MongoClientURI("mongodb://***REMOVED***:7117f92c295ab79aeb9e1303e1804752@***REMOVED***:***REMOVED***/***REMOVED***"))
+  val mongoConn = MongoClient(new MongoClientURI("mongodb://***REMOVED***:***REMOVED***@***REMOVED***:***REMOVED***/***REMOVED***"))
   val usersColl = mongoConn("***REMOVED***")("users")
   val gamesColl = mongoConn("***REMOVED***")("games")
   val facebookAuthColl = mongoConn("***REMOVED***")("facebook_autherizations")
