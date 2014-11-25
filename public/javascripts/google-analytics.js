187c7c043a9fe40b13620f2947406194f2e68c26
Jose Sanchez <jsanchez@gilt.com>
2014-11-25 10:30:28 -0500

    Fix for GA

--- public/javascripts/google-analytics.js
+++ public/javascripts/google-analytics.js
@@ -1,10 +1,22 @@
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-57086711-1', 'auto');
-  ga('send', 'pageview');
+// Initialize google analytics
+(function(i,s,o,g,r,a,m) {
 
-</script>
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  },
+
+  i[r].l=1*new Date();
+  a = s.createElement(o),
+
+  m = s.getElementsByTagName(o)[0];
+  a.async=1;
+  a.src = g;
+  m.parentNode.insertBefore(a,m)
+
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+// Add tracking information
+ga('create', 'UA-57086711-1', 'auto');
+ga('send', 'pageview');
