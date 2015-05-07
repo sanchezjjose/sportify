a77c4d1a45a6fe718ce72a36ae4de31b7dcf9b05
Jose Sanchez <jsanchez@gilt.com>
2015-05-07 00:38:17 -0400

    Using SendGrid API to deliver emails

--- app/models/MailScheduler.scala
+++ app/models/MailScheduler.scala
@@ -1,13 +1,12 @@
 package models
 
-import java.util.Properties
 import java.util.concurrent.{Executors, TimeUnit}
-import javax.mail.internet._
-import javax.mail.{PasswordAuthentication, _}
 
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.commons.MongoDBObject
 import com.novus.salat._
+import com.sendgrid.SendGrid
+import com.sendgrid.SendGrid.Email
 import controllers._
 import models.CustomPlaySalatContext._
 import org.joda.time.DateTime
@@ -71,60 +70,38 @@ class MailScheduler extends Loggable with Helper {
  */
 class MailSender extends Loggable with Config {
 
-  private val SMTP_HOST_NAME = Config.sendGridHost;
-  private val SMTP_AUTH_USER = Config.sendGridUsername;
-  private val SMTP_AUTH_PWD  = Config.sendGridPassword;
+  private val SMTP_AUTH_USER = Config.sendGridUsername
+  private val SMTP_AUTH_PWD  = Config.sendGridPassword
+
+  private val sendGrid = new SendGrid(SMTP_AUTH_USER, SMTP_AUTH_PWD)
 
   def sendNextGameReminderEmail(emailMessage: EmailMessage, team: Team, game: Game) {
 
     try {
-      val props = new Properties()
-      props.put("mail.transport.protocol", "smtp")
-      props.put("mail.smtp.host", SMTP_HOST_NAME)
-      props.put("mail.smtp.port", "587")
-      props.put("mail.smtp.auth", "true")
-
-      val auth = new SMTPAuthenticator()
-      val mailSession = Session.getInstance(props, auth)
-
-      val transport = mailSession.getTransport
-      val message = new MimeMessage(mailSession)
-      val multipart = new MimeMultipart("alternative")
-
-      val userId = emailMessage.user_id
       val recipient = emailMessage.recipient
 
-      val shouldSendEmail = {
+      val isEmailable = {
         (Config.environment == Environment.DEVELOPMENT && recipient == Config.testEmail) ||
          Config.environment == Environment.PRODUCTION
       }
 
-      log.info("Should send email to %s? %s".format(recipient, shouldSendEmail))
+      log.debug(s"Email $recipient: $isEmailable")
 
-      // Only send emails to me if in development
-      if (shouldSendEmail) {
-
-        val playersIn = game.players_in.map { playerId =>
-          "- " + User.findByPlayerId(playerId).get.first_name
-        }
-
+      if (isEmailable) {
+        val playersIn = game.players_in.map("- " + User.findByPlayerId(_).get.first_name)
+        val userId = emailMessage.user_id
         val html = views.html.email.reminderEmail(team, game, userId, playersIn).toString()
 
-        val part = new MimeBodyPart()
-        part.setContent(html, "text/html")
+        val email = new Email()
+        email.addSmtpApiTo(recipient)
+        email.setFrom(Config.fromEmail)
+        email.setSubject("You have an upcoming game on " + game.start_time)
+        email.setHtml(html)
 
-        multipart.addBodyPart(part)
+        val response = sendGrid.send(email)
 
-        message.setContent(multipart)
-        message.setFrom(new InternetAddress(Config.fromEmail))
-        message.setSubject("You have an upcoming game on " + game.start_time)
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient))
-
-        log.info("Sending an email to " + recipient + " for game id " + game._id)
-
-        transport.connect()
-        transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO))
-        transport.close()
+        log.info(s"Response : ${response.getMessage}")
+        log.info(s"Sending an email to $recipient for game id ${game._id}")
 
         EmailMessage.markAsSent(emailMessage)
       }
@@ -138,16 +115,6 @@ class MailSender extends Loggable with Config {
           EmailMessage.updateNumAttempts(emailMessage)
         }
       }
-    }
-  }
-
-  class SMTPAuthenticator extends javax.mail.Authenticator {
-    override def getPasswordAuthentication: PasswordAuthentication = {
-
-      val username = SMTP_AUTH_USER
-      val password = SMTP_AUTH_PWD
-
-      new PasswordAuthentication(username, password)
     }
   }
 }
